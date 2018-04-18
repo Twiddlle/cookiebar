a8d9f755d83f08e5a257871fe63c125c7939b599
Vladimir Durana <durana.vladimir99@gmail.com>
2018-04-18 22:31:02 +0200

    + export module

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'index.js',
+        library: 'library',
+        libraryTarget: 'commonjs-module'
     },
     module: {
         rules: [
